f96da65cbe78fe1a243fbe205dc4d0a89c9fbf8d
Sam Denijs <164017@student.horizoncollege.nl>
2024-09-20 11:01:25 +0200

    settings and urls

--- NIOZ/NIOZ/settings.py
+++ NIOZ/NIOZ/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'LoginSysteem',
+    'adminMenu',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +53,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'NIOZ.urls'
+
+import os
 
 TEMPLATES = [
     {
@@ -115,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
