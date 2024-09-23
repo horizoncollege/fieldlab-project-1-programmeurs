7936a8e08e8a80aadf4c0cb4df7537798094dc00
Sam Denijs <164017@student.horizoncollege.nl>
2024-09-23 10:28:49 +0200

    small fix
    proberen het probleem op te lossen van het niet willen werken van het project

--- NIOZ/NIOZ/settings.py
+++ NIOZ/NIOZ/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'LoginSysteem',
     'adminMenu',
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +53,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'NIOZ.urls'
+
+import os
 
 TEMPLATES = [
     {
@@ -118,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
