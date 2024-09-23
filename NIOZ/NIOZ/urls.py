5b7c8157612c3a884a4da89a1829c6a28ecbde41
Sam Denijs <164017@student.horizoncollege.nl>
2024-09-23 10:52:45 +0200

    bug fix
    probleem opgelost dat mijn pagina niet zichtbaar is.

--- NIOZ/NIOZ/urls.py
+++ NIOZ/NIOZ/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('LoginSysteem.urls')),
+    # path('login/', include('LoginSysteem.urls')),
     path('adminMenu/', include('adminMenu.urls')),
     
 ]
