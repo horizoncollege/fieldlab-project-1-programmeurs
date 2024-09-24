82f2bcded82ce75573d9107e6dc7ca3e69505c1b
Sam Denijs <164017@student.horizoncollege.nl>
2024-09-24 11:23:23 +0200

    test database build and making of te background code

--- NIOZ/adminMenu/views.py
+++ NIOZ/adminMenu/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def admin_menu_view(request):
     return render(request, 'adminMenu/adminMenu.html')
+
+def hardcoded_people_view(request):
+#hard coded data for now until database is accessible
+    people = [
+        {'Username': 'Alice154', 'realName': 'Alice', 'collectlocation': 'Texel (RW) Lauwersoog (RW)', 'yearFrom': 2004, 'yearUntil': '∞'},
+        {'Username': 'BobTheMan', 'realName': 'Bob', 'collectlocation': 'Texel (RW) Lauwersoog (RW)', 'yearFrom': 2014, 'yearUntil': '∞'},
+        {'Username': 'CharlieTheGoat', 'realName': 'Charlie', 'collectlocation': 'Texel (RW) Lauwersoog (RW)', 'yearFrom': 1997, 'yearUntil': '∞'},
+        {'Username': 'Diana372', 'realName': 'Diana', 'collectlocation': 'Texel (RW) Lauwersoog (RW)', 'yearFrom': 2018, 'yearUntil': '∞'},
+        {'Username': 'EthanBoy', 'realName': 'Ethan', 'collectlocation': 'Texel (RW) Lauwersoog (RW)', 'yearFrom': 1999, 'yearUntil': '∞'},
+    ]
+
+    return render(request, 'adminMenu/adminMenu.html', {'people': people})
