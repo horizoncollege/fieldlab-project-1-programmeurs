08c3e177d5f07bb5a9f4b0a2b806500d73d97b93
Sam Denijs <164017@student.horizoncollege.nl>
2024-09-26 14:25:31 +0200

    update to the home page and some small bug fixes

--- NIOZ/LoginSysteem/apps.py
+++ NIOZ/LoginSysteem/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
 
-
-<<<<<<<< HEAD:NIOZ/adminMenu/apps.py
 class AdminmenuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'adminMenu'
-========
+
 class LoginsysteemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'LoginSysteem'
->>>>>>>> LoginSysteem:NIOZ/LoginSysteem/apps.py
