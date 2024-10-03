498a2486da62386838ea23db8e66d61e8f7f464a
Sam Denijs <164017@student.horizoncollege.nl>
2024-10-03 12:30:36 +0200

    start database conection for administrator page

--- NIOZ/adminMenu/models.py
+++ NIOZ/adminMenu/models.py
@@ -1,8 +1,14 @@
+from django.shortcuts import render
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Person(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
+    username = models.CharField(max_length=50)
+    active = models.CharField(max_length=1, validators=[RegexValidator(r'^[01]$', 'Only "0" or "1" are allowed.')])
+    realName = models.CharField(max_length=50)
+    collectlocation = models.CharField(max_length=50)
+    yearFrom = models.CharField(max_length=10, validators=[RegexValidator(r'^\d+$', 'Only numeric characters are allowed.')])
+    yearUntil = models.CharField(max_length=10)
 
-    def __str__(self):
-        return self.name
+def __str__(self):
+        return f'{self.username} {self.active} {self.realName} {self.collectlocation} {self.yearFrom} {self.yearUntil}'
