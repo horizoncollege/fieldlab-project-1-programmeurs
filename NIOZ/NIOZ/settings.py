2dbe9a2e845be7c0a3f87939ff155d75c780540c
Marijn Blom <165215@student.horizoncollege.nl>
2024-10-04 11:23:14 +0200

    Fyke en Maintenance pagina's toegevoegd

--- NIOZ/NIOZ/settings.py
+++ NIOZ/NIOZ/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'LoginSysteem',
     'adminMenu',
+    'fyke',
+    'maintenance',
 ]
 
 MIDDLEWARE = [
--- NIOZ/NIOZ/urls.py
+++ NIOZ/NIOZ/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('LoginSysteem.urls')),
     path('', include('adminMenu.urls')),
+    path('fyke/', include('fyke.urls')),
+    path('maintenance/', include('maintenance.urls')),
 ]
