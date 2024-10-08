606ffc6035f6d1c5901ad5ae733002450f34730b
Marijn Blom <165215@student.horizoncollege.nl>
2024-10-08 09:55:11 +0200

    Fykeapp pagina's added

--- NIOZ/fyke/urls.py
+++ NIOZ/fyke/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('datacollection/', views.datacollection, name='datacollection'),
+    path('fishdetails/', views.fishdetails, name='fishdetails'),
+    path('fykelocations/', views.fykelocations, name='fykelocations'),
+    path('exportdata/', views.exportdata, name='exportdata'),
 ]
--- NIOZ/fyke/views.py
+++ NIOZ/fyke/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'fyke/index.html')
+
+def datacollection(request):
+    return render(request, 'fyke/datacollection.html')
+def fishdetails(request):
+    return render(request, 'fyke/fishdetails.html')
+def fykelocations(request):
+    return render(request, 'fyke/fykelocations.html')
+def exportdata(request):
+    return render(request, 'fyke/exportdata.html')
