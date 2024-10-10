eb7b17c25b5cdda4be97b114a321fb9cff02b50a
Nils Vos <157572@student.horizoncollege.nl>
2024-10-10 13:55:01 +0200

    Maintenance pagina aangemaakt

--- NIOZ/maintenance/urls.py
+++ NIOZ/maintenance/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('species_list/', views.species_list, name='species_list'),
+    path('fishprogrammes/', views.fishprogrammes, name='fishprogrammes'),
+    path('fishlocations/', views.fishlocations, name='fishlocations'),
 ]
--- NIOZ/maintenance/views.py
+++ NIOZ/maintenance/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'maintenance/index.html')
+
+def species_list(request):
+    return render(request, 'maintenance/species_list.html')
+def fishprogrammes(request):
+    return render(request, 'maintenance/fishprogrammes.html')
+def fishlocations(request):
+    return render(request, 'maintenance/fishlocations.html')
