3940e7ff82d4dd1ba7c48c95e9898d48e16d355f
Nils Vos <157572@student.horizoncollege.nl>
2024-10-22 10:42:02 +0200

    Model en css aangepast

--- NIOZ/adminMenu/models.py
+++ NIOZ/adminMenu/models.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.db import models
 from django.core.validators import RegexValidator
 
@@ -9,29 +8,30 @@ class Person(models.Model):
     collectlocation = models.CharField(max_length=50)
     yearFrom = models.CharField(max_length=10, validators=[RegexValidator(r'^\d+$', 'Only numeric characters are allowed.')])
     yearUntil = models.CharField(max_length=10)
-    accessTexel = models.IntegerField(max_length=1)
-    accessLauwersoog = models.IntegerField(max_length=1)
-    fishdata = models.IntegerField(max_length=1)
-    deleteRecords = models.IntegerField(max_length=1)
-    fishdataExport = models.IntegerField(max_length=1)
-    fishdataRecords = models.IntegerField(max_length=1)
-    fishdataSource = models.IntegerField(max_length=1)
-    fyke = models.IntegerField(max_length=1)
-    fykeBioticdata = models.IntegerField(max_length=1)
-    fykeDatacollection = models.IntegerField(max_length=1)
-    fykeExportdata = models.IntegerField(max_length=1)
-    fykeFishDetails = models.IntegerField(max_length=1)
-    fykeLocations = models.IntegerField(max_length=1)
-    help = models.IntegerField(max_length=1)
-    maintenance = models.IntegerField(max_length=1)
-    maintenanceFishprogrammes = models.IntegerField(max_length=1)
-    maintenanceLocations = models.IntegerField(max_length=1)
-    maintenanceSpecies = models.IntegerField(max_length=1)
-    manager = models.IntegerField(max_length=1)
-    managerUserAccess = models.IntegerField(max_length=1)
-    options = models.IntegerField(max_length=1)
-    optionsUserSettings = models.IntegerField(max_length=1)
+    
+    # Remove max_length from IntegerField
+    accessTexel = models.IntegerField()
+    accessLauwersoog = models.IntegerField()
+    fishdata = models.IntegerField()
+    deleteRecords = models.IntegerField()
+    fishdataExport = models.IntegerField()
+    fishdataRecords = models.IntegerField()
+    fishdataSource = models.IntegerField()
+    fyke = models.IntegerField()
+    fykeBioticdata = models.IntegerField()
+    fykeDatacollection = models.IntegerField()
+    fykeExportdata = models.IntegerField()
+    fykeFishDetails = models.IntegerField()
+    fykeLocations = models.IntegerField()
+    help = models.IntegerField()
+    maintenance = models.IntegerField()
+    maintenanceFishprogrammes = models.IntegerField()
+    maintenanceLocations = models.IntegerField()
+    maintenanceSpecies = models.IntegerField()
+    manager = models.IntegerField()
+    managerUserAccess = models.IntegerField()
+    options = models.IntegerField()
+    optionsUserSettings = models.IntegerField()
 
-
-def __str__(self):
+    def __str__(self):
         return f'{self.username} {self.active} {self.realName} {self.collectlocation} {self.yearFrom} {self.yearUntil} {self.accessTexel} {self.accessLauwersoog} {self.fishdata} {self.deleteRecords} {self.fishdataExport} {self.fishdataRecords} {self.fishdataSource} {self.fyke} {self.fykeBioticdata}{self.fykeDatacollection} {self.fykeExportdata} {self.fykeFishDetails} {self.fykeLocations} {self.help} {self.maintenance} {self.maintenanceFishprogrammes} {self.maintenanceLocations}{self.maintenanceSpecies} {self.manager} {self.managerUserAccess} {self.options} {self.optionsUserSettings}'
--- NIOZ/maintenance/urls.py
+++ NIOZ/maintenance/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('species_list/', views.species_list, name='species_list'),
     path('species/<int:species_id>/', views.species_detail, name='species_detail'),
-    path('species/edit/<int:species_id>/', views.species_edit, name='species_edit'),  # Zorg ervoor dat deze regel aanwezig is
+    path('species/edit/<int:species_id>/', views.species_edit, name='species_edit'),
     path('fishprogrammes/', views.fishprogrammes, name='fishprogrammes'),
     path('fishlocations/', views.fishlocations, name='fishlocations'),
 ]
